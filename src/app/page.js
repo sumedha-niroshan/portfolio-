5f15db2c7343588524def6f5f1611a1994e2519f
sumedha-Niroshan <sumedhan199@gmail.com>
2024-12-27 11:54:34 +0530

    page.js resolved #5

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import HeroSection from "./[components]/HeroSection";
-import NavBar from "./[components]/NavBar";
+import HeaderSection from "./[components]/HeaderSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col z-50">
-      <NavBar />
+      <HeaderSection />
+
       <div className="container mt-24 mx-auto px-12 py-4b z-5 ">
         <HeroSection />
       </div>
