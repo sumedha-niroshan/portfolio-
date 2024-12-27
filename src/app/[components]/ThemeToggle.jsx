9c671d8cacc0c9d099a034e7941a59f73c021b6b
sumedha-Niroshan <sumedhan199@gmail.com>
2024-12-27 07:52:52 +0530

    ajust thecolor in toggle button

--- src/app/[components]/ThemeToggle.jsx
+++ src/app/[components]/ThemeToggle.jsx
@@ -25,7 +25,7 @@ function ThemeToggle() {
   return (
     <button
       onClick={toggleTheme}
-      className="p-2 rounded-full bg-gray-200 dark:bg-gray-800 text-gray-800 dark:text-gray-200 transition-colors duration-300 focus:outline-none"
+      className="p-2 rounded-full bg-gray-200 dark:bg-gray-500 text-gray-800 dark:text-gray-200 transition-colors duration-300 focus:outline-none"
     >
       {theme === "dark" ? (
         <SunIcon className="h-6 w-6" />
