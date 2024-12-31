c9edffd93a18b5b2128ffe51c1c6820e0f60c4db
sumedha-Niroshan <sumedhan199@gmail.com>
2025-01-01 01:48:28 +0530

    footer created

--- src/app/page.js
+++ src/app/page.js
@@ -19,8 +19,9 @@ export default function Home() {
         <SkillSection />
         <EducationSection />
         <AboutMeSection />
+        <Footer />
       </div>
-      <Footer />
+      
     </main>
   );
 }
